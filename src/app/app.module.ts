512265a715d4cbcbdebe3dfb6641162dcc16d3af
steviep <ig2bagd@yahoo.com>
2021-09-06 21:36:45 -0700

    Add @ngrx/store-devtools & @ngrx/router-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
-import { Effect, EffectsModule } from '@ngrx/effects';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -13,6 +15,8 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import * as fromApp from './store/app.reducer';
 import { AuthEffects } from './auth/store/auth.effects';
+import { environment } from 'src/environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -29,6 +33,9 @@ import { AuthEffects } from './auth/store/auth.effects';
     // ShoppingListModule,              "     "
     StoreModule.forRoot(fromApp.appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({ logOnly: environment.production }),
+    StoreRouterConnectingModule.forRoot(),
+    StoreDevtoolsModule,
     SharedModule,
     CoreModule
   ],
